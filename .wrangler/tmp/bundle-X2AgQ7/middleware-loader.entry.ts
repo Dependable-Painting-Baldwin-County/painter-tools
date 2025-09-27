24afd448c18d7d2438a3ead25487522f20c0f788
Alex Dimmler Dependable Painting <alexdimmler@dependablepainting.work>
2025-09-27 03:59:02 -0500

    update

--- .wrangler/tmp/bundle-X2AgQ7/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-X2AgQ7/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/alexdimmler/Website/painter-tools/.wrangler/tmp/bundle-rg3xYq/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/alexdimmler/Website/painter-tools/.wrangler/tmp/bundle-X2AgQ7/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/alexdimmler/Library/pnpm/global/5/.pnpm/wrangler@4.40.1/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/alexdimmler/Website/painter-tools/.wrangler/tmp/bundle-rg3xYq/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/alexdimmler/Website/painter-tools/.wrangler/tmp/bundle-X2AgQ7/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/alexdimmler/Website/painter-tools/.wrangler/tmp/bundle-rg3xYq/middleware-insertion-facade.js";
+export * from "/Users/alexdimmler/Website/painter-tools/.wrangler/tmp/bundle-X2AgQ7/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
--- .wrangler/tmp/dev-LpeiA8/index.js
+++ .wrangler/tmp/dev-LpeiA8/index.js
@@ -2069,7 +2069,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env2, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-9y7NPi/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-X2AgQ7/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -2101,7 +2101,7 @@ function __facade_invoke__(request, env2, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-9y7NPi/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-X2AgQ7/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
